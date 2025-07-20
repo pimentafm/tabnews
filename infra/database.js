0b4cc498d4d7ed04cb1ec57926b8210bd0f29693
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2025-07-20 19:14:40 -0300

    Adds `/api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -10,18 +10,10 @@ function getSSLValues() {
 }
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: String(process.env.POSTGRES_PASSWORD),
-    pool_mode: process.env.POSTGRES_POOL_MODE || "session",
-    ssl: getSSLValues(),
-  });
+  let client;
 
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
@@ -32,6 +24,22 @@ async function query(queryObject) {
   }
 }
 
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: String(process.env.POSTGRES_PASSWORD),
+    pool_mode: process.env.POSTGRES_POOL_MODE || "session",
+    ssl: getSSLValues(),
+  });
+
+  await client.connect();
+  return client;
+}
+
 export default {
-  query: query,
+  query,
+  getNewClient,
 };
