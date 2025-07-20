07ae85f73079d500ee0c2f776ea005939f079a20
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2025-07-20 18:24:45 -0300

    Configure Jest to transpile ESM

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,3 +1,10 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+async function cleanDatabase() {
+  await database.query("DROP schema public CASCADE; CREATE schema public;");
+}
+
 test("GET to /api/v1/migrations should return status 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
@@ -5,4 +12,5 @@ test("GET to /api/v1/migrations should return status 200", async () => {
   const responseBody = await response.json();
 
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 });
